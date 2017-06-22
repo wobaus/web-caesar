7654613436f1b5fd0b7d658de2b7827e3892f475
Sun Baus <wobaus@gmail.com>
2017-06-21 23:14:40 -0700

    delete unnecessary line

--- main.py
+++ main.py
@@ -11,14 +11,14 @@ form = """
 <html>
     <head>
         <style>
-            form {
+            form {{
                 background-color: #eee;
                 padding: 20px;
                 margin: 0 auto;
                 width: 540px;
                 font: 16px sans-serif;
                 border-radius: 10px;
-                }
+                }}
             textarea {{
                 margin: 10px 0;
                 width: 540px;
@@ -40,18 +40,15 @@ form = """
 
 @app.route("/")
 def index():
-    text = request.args.get('text')
-    return form
+    
+    return form.format('')
 
  
 @app.route("/", methods=['POST'])
 def encrypt():
-    rot = request.form['rot']
-    rot = int(rot)
+    rot = int(request.form['rot'])
     text = request.form['text']
-    encrypted = rotate_string(text, rot)
-    #encrypted = form.format('encrypted')
-    
-    return  "<h1>" + encrypted + "</h1>"
+    encrypted = rotate_string(text, rot)    
+    return  "<h1>" + form.format(encrypted) + "</h1>"
 
 app.run()
